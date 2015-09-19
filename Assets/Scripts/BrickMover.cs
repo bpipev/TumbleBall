b8d198136a5c6941bce5f28836e20b601f001a03
Borislav Pipev <bobipipev@hotmail.com>
2015-09-19 17:01:11 -0400

    Updated BrikMover
    Now texture scales and walls are different sizes

--- Assets/Scripts/BrickMover.cs
+++ Assets/Scripts/BrickMover.cs
@@ -14,10 +14,29 @@ public class BrickMover : MonoBehaviour {
         foreach(GameObject brick in bricks)
         {
             Brick b = brick.GetComponent<Brick>();
-            Rigidbody rb = brick.GetComponent<Rigidbody>();
             if (!b.IsMoving)
             {
-                brick.transform.position = new Vector3(0f, -6f, 0f);
+                MeshRenderer m = brick.GetComponent<MeshRenderer>();
+                Rigidbody rb = brick.GetComponent<Rigidbody>();
+                float positive_or_negative = (Random.Range(0, 2) * 2 - 1);
+                Vector3 position = new Vector3(0.5f + Random.Range(1, 6) * positive_or_negative, -6f, 0f);
+                brick.transform.position = position;
+                Vector3 scaleBy = new Vector3();
+                if (brick.transform.localScale.x >= 1f)
+                    brick.transform.localScale = new Vector3(0.1f, 1f, 0.05f);
+                else
+                {
+                    positive_or_negative = (Random.Range(0, 2) * 2 - 1);
+                    float width_scale = 0f;
+                    do
+                    {
+                        positive_or_negative = (Random.Range(0, 2) * 2 - 1);
+                        width_scale = 0.1f * Random.Range(0, 10) * positive_or_negative;
+                    } while (width_scale < 0.1f);
+                    scaleBy = new Vector3(width_scale, 0f, 0f);
+                }
+                brick.transform.localScale += scaleBy;
+                m.material.mainTextureScale += new Vector2(scaleBy.x * 2 * 10, scaleBy.y);
                 b.IsMoving = true;
                 rb.AddForce(new Vector3(0f, 100f, 0f));
                 break;
