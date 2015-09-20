c8ea1947adeb59357034b7a1d5e0e1ba59361fa4
Andrew <andrew.jias@gmail.com>
2015-09-19 20:29:53 -0400

    prelim work on scrolling background

---
+++ Assets/Scripts/BgScroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+		Material mat = mr.material;
+		Vector2 offset = mat.mainTextureOffset;
+		offset.x = transform.position.x / transform.localScale.x;
+		offset.y = transform.position.y / transform.localScale.y;
+
+		mat.mainTextureOffset = offset;
+	}
+}
