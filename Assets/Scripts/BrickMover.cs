c5836a7787ff027e4878a38b55ea956c2cfb9493
Borislav Pipev <bobipipev@hotmail.com>
2015-09-21 22:12:00 -0400

    Added collisions

--- Assets/Scripts/BrickMover.cs
+++ Assets/Scripts/BrickMover.cs
@@ -17,7 +17,7 @@ public class BrickMover : MonoBehaviour {
             if (!b.IsMoving)
             {
                 MeshRenderer m = brick.GetComponent<MeshRenderer>();
-                Rigidbody rb = brick.GetComponent<Rigidbody>();
+                Rigidbody2D rb = brick.GetComponent<Rigidbody2D>();
                 float positive_or_negative = (Random.Range(0, 2) * 2 - 1);
                 Vector3 position = new Vector3(0.5f + Random.Range(1, 6) * positive_or_negative, -6f, 0f);
                 brick.transform.position = position;
@@ -49,7 +49,7 @@ public class BrickMover : MonoBehaviour {
         foreach (GameObject brick in bricks)
         {
             Brick b = brick.GetComponent<Brick>();
-            Rigidbody rb = brick.GetComponent<Rigidbody>();
+            Rigidbody2D rb = brick.GetComponent<Rigidbody2D>();
             Renderer r = brick.GetComponent<Renderer>();
             if (b.IsMoving && !r.isVisible && brick.transform.position.y > 0f)
             {
@@ -57,5 +57,11 @@ public class BrickMover : MonoBehaviour {
                 rb.velocity = new Vector3();
             }
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.tag == "Wall")
+            //collision.gameObject.ve
     }
 }
