5aa5e0595fb52dde3806bc71498361afeccb4eea
Borislav Pipev <bobipipev@hotmail.com>
2015-09-23 23:46:33 -0400

    wall material fix

--- Assets/Scripts/BrickMover.cs
+++ Assets/Scripts/BrickMover.cs
@@ -22,21 +22,26 @@ public class BrickMover : MonoBehaviour {
                 Vector3 position = new Vector3(0.5f + Random.Range(1, 6) * positive_or_negative, -6f, 0f);
                 brick.transform.position = position;
                 Vector3 scaleBy = new Vector3();
+                float width_scale = 0f;
                 if (brick.transform.localScale.x >= 1f)
+                {
                     brick.transform.localScale = new Vector3(0.1f, 1f, 0.05f);
+                    width_scale = 0.1f;
+                    m.material.mainTextureScale = new Vector2(2f, 2f);
+                }
                 else
                 {
                     positive_or_negative = (Random.Range(0, 2) * 2 - 1);
-                    float width_scale = 0f;
                     do
                     {
                         positive_or_negative = (Random.Range(0, 2) * 2 - 1);
                         width_scale = 0.1f * Random.Range(0, 10) * positive_or_negative;
                     } while (width_scale < 0.1f);
                     scaleBy = new Vector3(width_scale, 0f, 0f);
+                    brick.transform.localScale += scaleBy;
+                    m.material.mainTextureScale += new Vector2(scaleBy.x * 2 * 10, scaleBy.y);
                 }
-                brick.transform.localScale += scaleBy;
-                m.material.mainTextureScale += new Vector2(scaleBy.x * 2 * 10, scaleBy.y);
+                
                 b.IsMoving = true;
                 rb.AddForce(new Vector3(0f, 100f, 0f));
                 break;
