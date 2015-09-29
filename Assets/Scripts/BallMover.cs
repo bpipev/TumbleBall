b920b3a702824a32cba6b3680a7a7e62e5fbc3f9
Andrew <andrew.jias@gmail.com>
2015-09-28 22:56:35 -0400

    added accelerometer movement and tap jump
    Still need rebalancing

---
+++ Assets/Scripts/BallMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMover : MonoBehaviour {
+	
+	public float rollingSpeed;
+	public float jumpHeight;
+	public float direction;
+	// Update is called once per frame
+	void Update () {
+		direction = Input.acceleration.x * rollingSpeed;
+		GetComponent<Rigidbody2D>().AddForce (new Vector2(direction,0));
+		
+		foreach (Touch touch in Input.touches) {
+			GetComponent<Rigidbody2D>().AddForce (new Vector2(0,jumpHeight));
+		}
+		
+		
+	}
+}
