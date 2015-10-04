fad2fc5e98c5e3c758b9afc57128a27b4cdc8244
Borislav Pipev <bobipipev@hotmail.com>
2015-10-04 17:48:56 -0400

    move side boundaries to sides of screen

---
+++ Assets/Scripts/SetLeftBoundary.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetLeftBoundary : MonoBehaviour {
+
+    public bool IsLeftBoundary { get; set; }
+	// Use this for initialization
+	void Start () {
+        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        Vector2 left_side = Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5f, camDistance));
+        this.transform.position = new Vector3(left_side.x, 0, 0) - new Vector3(this.transform.localScale.x/2, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SetRightBoundary.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetRightBoundary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        Vector2 left_side = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0.5f, camDistance));
+        this.transform.position = new Vector3(left_side.x, 0, 0) +- new Vector3(this.transform.localScale.x / 2, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
