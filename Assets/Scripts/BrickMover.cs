05c09417158d28d4f6cdd2232413da45cae53c9a
Borislav Pipev <bobipipev@hotmail.com>
2015-10-04 16:58:36 -0400

    some refactoring

--- Assets/Scripts/BrickMover.cs
+++ Assets/Scripts/BrickMover.cs
@@ -2,60 +2,29 @@
 using System.Collections;
 
 public class BrickMover : MonoBehaviour {
-    GameObject[] bricks;
+    GameObject[] wall_parents;
     public float brick_start_position;
-    public float brick_max_width_scale;
+    
     // Use this for initialization
     void Start () {
-        bricks = GameObject.FindGameObjectsWithTag("Parent_brick");
-        InvokeRepeating("MoveBrick", 0f, 1f);
+        wall_parents = GameObject.FindGameObjectsWithTag("Parent_brick");
+        InvokeRepeating("MoveWall", 0f, 1f);
     }
 
-    void MoveBrick()
+    void MoveWall()
     {
-        foreach(GameObject brick in bricks)
+        foreach(GameObject wall_parent in wall_parents)
         {
-            Brick b = brick.GetComponent<Brick>();
-            if (!b.IsMoving)
+            Wall wall = wall_parent.GetComponentInChildren<Wall>();
+            if (!wall.IsMoving)
             {
-                MeshRenderer m = brick.GetComponentInChildren<MeshRenderer>();
-                Rigidbody2D rb = brick.GetComponent<Rigidbody2D>();
+                Rigidbody2D rb = wall_parent.GetComponent<Rigidbody2D>();
                 float positive_or_negative = (Random.Range(0, 2) * 2 - 1);
                 Vector3 position = new Vector3((float)Random.Range(0, 31)/10f * positive_or_negative, brick_start_position, 0f);
-                brick.transform.position = position;
-                Vector3 scaleBy = new Vector3();
-                float width_scale = 0f;
-                var wall = brick.transform.Find("Wall");
-                float next_width_scale = wall.transform.localScale.x * 100 + width_scale;
-                do
-                {
-                    if (next_width_scale >= brick_max_width_scale)
-                    {
-                        width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100) )) * -1f;
-                    }
-                    else if (next_width_scale < 10f)
-                    {
-                        width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100)));
-                    }
-                    else
-                    {
-                        positive_or_negative = (Random.Range(0, 2) * 2 - 1);
-                        if(positive_or_negative == -1f)
-                        {
-                            width_scale = Random.Range(1, (int)((wall.transform.localScale.x * 100 - 10f))) * positive_or_negative;
-                        }
-                        else
-                        {
-                            width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100))) * positive_or_negative;
-                        }
-                    }
-                    next_width_scale = wall.transform.localScale.x * 100 + width_scale;
-                } while (next_width_scale < 10f || next_width_scale >= brick_max_width_scale);
-                scaleBy = new Vector3(0.01f * width_scale, 0f, 0f);
-                wall.transform.localScale += scaleBy;
-                m.material.mainTextureScale += new Vector2(scaleBy.x * 2 * 10, scaleBy.y);
+                wall_parent.transform.position = position;
                 rb.velocity = new Vector3();
-                b.IsMoving = true;
+                wall.ScaleWall();
+                wall.IsMoving = true;
                 rb.AddForce(new Vector3(0f, 100f, 0f));
                 break;
             }
@@ -64,14 +33,14 @@ public class BrickMover : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        foreach (GameObject brick in bricks)
+        foreach (GameObject wall_parent in wall_parents)
         {
-            Brick b = brick.GetComponent<Brick>();
-            Rigidbody2D rb = brick.GetComponent<Rigidbody2D>();
-            Renderer r = brick.GetComponentInChildren<Renderer>();
-            if (b.IsMoving && !r.isVisible && brick.transform.position.y > 0f)
+            Wall wall = wall_parent.GetComponentInChildren<Wall>();
+            Rigidbody2D rb = wall_parent.GetComponent<Rigidbody2D>();
+            Renderer r = wall_parent.GetComponentInChildren<Renderer>();
+            if (wall.IsMoving && !r.isVisible && wall_parent.transform.position.y > 0f)
             {
-                b.IsMoving = false;
+                wall.IsMoving = false;
                 rb.velocity = new Vector3();
             }
         }
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+    public bool IsMoving { get; set; }
+    public float brick_max_width_scale;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public void ScaleWall()
+    {
+            float positive_or_negative = 1f;
+            Vector3 scaleBy = new Vector3();
+            MeshRenderer m = this.GetComponentInChildren<MeshRenderer>();
+            float width_scale = 0f;
+            var wall = this;
+            float next_width_scale = wall.transform.localScale.x * 100 + width_scale;
+            do
+            {
+                if (next_width_scale >= brick_max_width_scale)
+                {
+                    width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100))) * -1f;
+                }
+                else if (next_width_scale < 10f)
+                {
+                    width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100)));
+                }
+                else
+                {
+                    positive_or_negative = (Random.Range(0, 2) * 2 - 1);
+                    if (positive_or_negative == -1f)
+                    {
+                        width_scale = Random.Range(1, (int)((wall.transform.localScale.x * 100 - 10f))) * positive_or_negative;
+                    }
+                    else
+                    {
+                        width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100))) * positive_or_negative;
+                    }
+                }
+                next_width_scale = wall.transform.localScale.x * 100 + width_scale;
+            } while (next_width_scale < 10f || next_width_scale >= brick_max_width_scale);
+            scaleBy = new Vector3(0.01f * width_scale, 0f, 0f);
+            wall.transform.localScale += scaleBy;
+            m.material.mainTextureScale += new Vector2(scaleBy.x * 2 * 10, scaleBy.y);
+        }
+}
