b554cd27e956c07e8b101325be886cab0581a8f3
Borislav Pipev <bobipipev@hotmail.com>
2015-10-04 17:53:06 -0400

    small fix

--- Assets/Scripts/SetRightBoundary.cs
+++ Assets/Scripts/SetRightBoundary.cs
@@ -7,7 +7,7 @@ public class SetRightBoundary : MonoBehaviour {
 	void Start () {
         float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
         Vector2 left_side = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0.5f, camDistance));
-        this.transform.position = new Vector3(left_side.x, 0, 0) +- new Vector3(this.transform.localScale.x / 2, 0, 0);
+        this.transform.position = new Vector3(left_side.x, 0, 0) + new Vector3(this.transform.localScale.x / 2, 0, 0);
     }
 	
 	// Update is called once per frame
