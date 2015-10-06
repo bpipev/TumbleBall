92c0cc1d5ef63156b2303657402238fa58d9f09f
Borislav Pipev <bobipipev@hotmail.com>
2015-10-05 20:28:15 -0400

    started top and bottom boundaries

---
+++ Assets/Scripts/BoundaryScripts/SetTopBoundary.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetTopBoundary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        Vector2 top = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1f, camDistance));
+        this.transform.position = new Vector3(0, top.y, 0) + new Vector3(0, this.transform.localScale.x / 2, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+}
