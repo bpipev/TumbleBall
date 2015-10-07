69a7bc92865944127d9b9dd337876ef653b6d5a1
Borislav Pipev <bobipipev@hotmail.com>
2015-10-06 23:14:59 -0400

    stuff

--- Assets/Scripts/BoundaryScripts/SetBottomBoundary.cs
+++ Assets/Scripts/BoundaryScripts/SetBottomBoundary.cs
@@ -1,21 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SetTopBoundary : MonoBehaviour {
+public class SetBottomBoundary : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
         float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
-        Vector2 top = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1f, camDistance));
-        this.transform.position = new Vector3(0, top.y, 0) + new Vector3(0, this.transform.localScale.x, 0);
+        Vector2 top = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0f, camDistance));
+        this.transform.position = new Vector3(0, top.y, 0) - new Vector3(0, this.transform.localScale.x, 0);
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+	
 	}
 
-    void OnTriggerEnter2D(Collider2D coll)
+    void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
