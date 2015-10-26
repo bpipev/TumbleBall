7158693f75dcaf245a43173d8383651a3b4c2c14
Borislav Pipev <bobipipev@hotmail.com>
2015-10-25 20:33:35 -0400

    changed spike animation and simplified the wall scaling

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -22,8 +22,7 @@ public class Wall : MonoBehaviour {
 
         wall.transform.localScale = new Vector3(0.1f, 0f, 0.05f);
         m.material.mainTextureScale = new Vector2(0.1f * 2 * 10, 1f);
-
-        float positive_or_negative = 1f;
+        
         Vector3 scaleBy = new Vector3();
         
         float width_scale = 0f;
@@ -34,25 +33,8 @@ public class Wall : MonoBehaviour {
             brick_max_width_scale = 6f - ball_width * 2;
 
         brick_max_width_scale *= 10;
-
-        float next_width_scale = wall.transform.localScale.x * 100 + width_scale;
-        do
-        {
-            if (next_width_scale >= brick_max_width_scale)
-            {
-                width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100))) * -1f;
-            }
-            else if (next_width_scale < 10f)
-            {
-                width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100)));
-            }
-            else
-            {
-                positive_or_negative = (Random.Range(0, 2) * 2 - 1);
-                width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100))) * positive_or_negative;
-            }
-            next_width_scale = wall.transform.localScale.x * 100 + width_scale;
-        } while (next_width_scale < 10f || next_width_scale >= brick_max_width_scale);
+        
+        width_scale = Random.Range(1, (int)((brick_max_width_scale - wall.transform.localScale.x * 100)));
         scaleBy = new Vector3(0.01f * width_scale, 0f, 0f);
         wall.transform.localScale += scaleBy;
         m.material.mainTextureScale += new Vector2(scaleBy.x * 2 * 10, scaleBy.y);
