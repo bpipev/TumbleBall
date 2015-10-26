820680bad7bc42ab14a5d3e9419f1b7752ab9672
Borislav Pipev <bobipipev@hotmail.com>
2015-10-25 21:40:01 -0400

    forgot to add files

---
+++ Assets/Scripts/HittingTraps.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HittingTraps : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Spike")
+        {
+            GameLogic gl = GameObject.FindGameObjectWithTag("GameLogic").GetComponent<GameLogic>();
+            gl.GameOver();
+        }
+
+    }
+}
